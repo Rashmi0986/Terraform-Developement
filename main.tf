11ad99632834d05726e89249781563c38526fd61
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-01-23 07:17:45 +0000

    added key pair and subnet details

--- main.tf
+++ main.tf
@@ -5,4 +5,6 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-0a3c3a20c09d6f377"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0aa4846c85e3949c9"
+    key_name = "aws_login"
 }
