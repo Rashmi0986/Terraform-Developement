aa46836efc53bd0eab973fc16d643dd02ccf9c90
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-01-23 07:06:40 +0000

    adding main.tf file

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0a3c3a20c09d6f377"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+}
