e4c887ebf23d2505f4ed78dc7754313dbd4750aa
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-01-25 10:18:41 +0530

    Update variable.tf
    Added the subnet id

--- variable.tf
+++ variable.tf
@@ -26,4 +26,8 @@ variable "ami_id" {
   description = "Id of the ami to deploy, depends on region."
   default = "ami-0a3c3a20c09d6f377" // for region us-east-1
 }
+variable "subnet_id" {
+  description = "Id of the ami to deploy, depends on region."
+  default = "subnet-0aa4846c85e3949c9" // for region us-east-1
+}
 
